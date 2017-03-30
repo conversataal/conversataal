7c885054b68d3ff8f321bef0ebb9f8e4a336fa3e
m.van.es <m.van.es@everest.nl>
2017-03-30 13:40:37 +0200

    Updated content

--- conversa/pages/_template.jsx
+++ conversa/pages/_template.jsx
@@ -57,7 +57,7 @@ module.exports = React.createClass({
             <div className="row">
               <div className="col l6 s12">
                 <h5 className="white-text">Company Bio</h5>
-                <p className="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+                <p className="grey-text text-lighten-4">Polish language school.</p>
               </div>
               <div className="col l3 s12">
                 <h5 className="white-text">Example types</h5>
--- conversa/pages/index.js
+++ conversa/pages/index.js
@@ -34,9 +34,9 @@ export default class Index extends React.Component {
           <div className="section no-pad-bot">
             <div className="container">
               <br/><br/>
-              <h1 className="header center teal-text text-lighten-2">Parallax Template</h1>
+              <h1 className="header center teal-text text-lighten-2">Conversa</h1>
               <div className="row center">
-                <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                <h5 className="header col s12 light">Tailor-made individual and group courses</h5>
               </div>
               <div className="row center">
                 <a href="http://materializecss.com/getting-started.html" id="download-button" className="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
@@ -53,28 +53,28 @@ export default class Index extends React.Component {
             <div className="row">
               <div className="col s12 m4">
                 <div className="icon-block">
-                  <h2 className="center brown-text"><i className="material-icons">flash_on</i></h2>
-                  <h5 className="center">Speeds up development</h5>
+                  <h2 className="center brown-text"><i className="material-icons">business</i></h2>
+                  <h5 className="center">In your company</h5>
 
-                  <p className="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                  <p className="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. </p>
                 </div>
               </div>
 
               <div className="col s12 m4">
                 <div className="icon-block">
                   <h2 className="center brown-text"><i className="material-icons">group</i></h2>
-                  <h5 className="center">User Experience Focused</h5>
+                  <h5 className="center">At your chosen location</h5>
 
-                  <p className="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                  <p className="light">Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. </p>
                 </div>
               </div>
 
               <div className="col s12 m4">
                 <div className="icon-block">
-                  <h2 className="center brown-text"><i className="material-icons">settings</i></h2>
-                  <h5 className="center">Easy to work with</h5>
+                  <h2 className="center brown-text"><i className="material-icons">videocam</i></h2>
+                  <h5 className="center">On Skype</h5>
 
-                  <p className="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+                  <p className="light">Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
                 </div>
               </div>
             </div>
@@ -86,7 +86,7 @@ export default class Index extends React.Component {
           <div className="section no-pad-bot">
             <div className="container">
               <div className="row center">
-                <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                <h5 className="header col s12 light">Tailor-made individual and group courses</h5>
               </div>
             </div>
           </div>
@@ -112,7 +112,7 @@ export default class Index extends React.Component {
           <div className="section no-pad-bot">
             <div className="container">
               <div className="row center">
-                <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                <h5 className="header col s12 light">Tailor-made individual and group courses</h5>
               </div>
             </div>
           </div>
