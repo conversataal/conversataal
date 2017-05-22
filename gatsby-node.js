23fa9a4cce1822b34957c104069eaf4b53fbb83b
M.D. van Es <es@mdworld.nl>
2017-05-22 10:43:45 +0200

    Reset this branch

--- gatsby-node.js
+++
@@ -1,7 +0,0 @@
-exports.modifyWebpackConfig = function(config, stage) {
-  config.alias = {
-      'materialize': 'materialize-css/dist/js/materialize.min',
-      'materialize-css': 'materialize-css/dist/js/materialize.min'
-  };
-  return config
-}
