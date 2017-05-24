30aba086d6ab4fcd9f838ef33c4e056f3c2b4a47
M.D. van Es <es@mdworld.nl>
2017-05-24 18:55:59 +0200

    Footer colors

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -29,7 +29,7 @@ class CustomTemplate extends React.Component {
                  {this.props.children}
                  </Container>*/}
 
-                <footer className="page-footer teal">
+                <footer className="page-footer indigo darken-3">
                     <div className="container">
                         <div className="row">
                             <div className="col l6 s12">
@@ -72,9 +72,9 @@ class CustomTemplate extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <div className="footer-copyright">
+                    <div className="footer-copyright indigo darken-4">
                         <div className="container">
-                            Made by <a className="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+                            <a className="indigo-text text-darken-2" href="http://mdworld.nl">mdworld.nl</a>
                         </div>
                     </div>
                 </footer>
